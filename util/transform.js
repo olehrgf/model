251ee5fec73dac3e2b4b5f57d0a8c8f3b24c7682
Oleh Rohov <olehrgf@gmail.com>
2018-12-21 16:04:31 +0200

    migrate to ES6

--- util/transform.js
+++ util/transform.js
@@ -1,33 +1,30 @@
-
-module.exports.uppercase = function( val ){
-  return val.toUpperCase();
+module.exports.uppercase = function (val) {
+    return val.toUpperCase();
 };
 
-module.exports.lowercase = function( val ){
-  return val.toLowerCase();
+module.exports.lowercase = function (val) {
+    return val.toLowerCase();
 };
 
-module.exports.stringify = function( val ){
-  // because javascript: (''+undefined) === 'undefined'
-  if( 'undefined' === typeof val ){
-    return '';
-  }
-  return '' + val;
+module.exports.stringify = function (val) {
+    // because javascript: (''+undefined) === 'undefined'
+    if ('undefined' === typeof val) {
+        return '';
+    }
+    return '' + val;
 };
 
-module.exports.floatify = function( precision, val ){
-  return parseFloat( val ).toFixed( precision || 10 )/1;
+module.exports.floatify = function (precision, val) {
+    return parseFloat(val).toFixed(precision || 10) / 1;
 };
 
-module.exports.roundify = function( val ){
-  return Math.round(val);
+module.exports.roundify = function (val) {
+    return Math.round(val);
 };
 
-module.exports.toULLR = function( val ) {
-  return JSON.stringify({
-    min_lat: val.lowerRight.lat,
-    max_lat: val.upperLeft.lat,
-    min_lon: val.upperLeft.lon,
-    max_lon: val.lowerRight.lon
-  });
+module.exports.toULLR = function (val) {
+    return JSON.stringify({
+        type: 'envelope',
+        coordinates: [val.upperLeft.lon, val.upperLeft.lat, val.lowerRight.lon, val.lowerRight.lat]
+    });
 };
